f481ab1062f3b5f602d27c39b65761ef6f715a64
seunghoon Lee <hara9052@naver.com>
2024-01-22 22:56:33 +0900

    setting News, MyPage

---
+++ app/src/main/java/com/example/congress/base/MainVpAdapter.kt
@@ -0,0 +1,18 @@
+package com.example.congress.base
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.example.congress.mypage.MyPageFragment
+import com.example.congress.news.NewsFragment
+
+class MainVpAdapter(fa: FragmentActivity) : FragmentStateAdapter(fa) {
+    val fragments = listOf(NewsFragment(),MyPageFragment())
+    override fun getItemCount(): Int {
+        return fragments.size
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        return fragments[position]
+    }
+}
---
+++ app/src/main/java/com/example/congress/mypage/MyPageFragment.kt
@@ -0,0 +1,28 @@
+package com.example.congress.mypage
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.congress.R
+import com.example.congress.databinding.FragmentMyPageBinding
+import com.example.congress.databinding.FragmentNewsBinding
+
+class MyPageFragment : Fragment() {
+
+    private var _binding: FragmentMyPageBinding? = null
+    private val binding get() = _binding!!
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        _binding = FragmentMyPageBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+}
