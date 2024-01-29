2d5af9614df9112f16a6d6b3e7d35aeb5994d294
arinming <jyandysys1@naver.com>
2024-01-29 21:31:12 +0900

    [Feat] MyInfoActivity 구현 (#6)

---
+++ app/src/main/java/com/example/congress/presentation/ui/mypage/info/MyInfoActivity.kt
@@ -0,0 +1,51 @@
+package com.example.congress.presentation.ui.mypage.info
+
+import android.os.Bundle
+import android.widget.Toast
+import com.example.congress.R
+import com.example.congress.base.BaseActivity
+import com.example.congress.databinding.ActivityMyInfoBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MyInfoActivity : BaseActivity<ActivityMyInfoBinding>(R.layout.activity_my_info) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        initView()
+    }
+
+    override fun initView() {
+        super.initView()
+        selectRadioButton()
+        setupYearPicker()
+    }
+
+
+    private fun selectRadioButton() {
+        val radioGroup = binding.radioGroup
+
+        radioGroup.setOnCheckedChangeListener { group, checkedId ->
+            when (checkedId) {
+                R.id.radio_man -> {
+                    Toast.makeText(this, "남자", Toast.LENGTH_SHORT).show()
+                }
+                R.id.radio_woman -> {
+                    Toast.makeText(this, "여자", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+
+    private fun setupYearPicker() {
+        val yearPicker = binding.yearPicker
+        val currentYear = java.util.Calendar.getInstance().get(java.util.Calendar.YEAR)
+
+        yearPicker.minValue = 1900
+        yearPicker.maxValue = currentYear
+        yearPicker.value = currentYear
+
+    }
+
+
+}
---
+++ app/src/main/java/com/example/congress/presentation/ui/mypage/myAct/MyActActivity.kt
@@ -0,0 +1,19 @@
+package com.example.congress.presentation.ui.mypage.myAct
+
+import android.os.Bundle
+import com.example.congress.R
+import com.example.congress.base.BaseActivity
+import com.example.congress.databinding.ActivityMyActBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MyActActivity: BaseActivity<ActivityMyActBinding>(R.layout.activity_my_act) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun initView() {
+        super.initView()
+    }
+}
